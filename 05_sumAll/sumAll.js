5a5ef8bd5454787992d50653b33e4a58f69c409b
Dan <58478165+DKNTT@users.noreply.github.com>
2024-05-18 18:38:13 +0100

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+    if (typeof int1 !== 'number' || typeof int2 !== 'number') {
+        return 'ERROR'
+    }
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR'
+    }
 
+    if (int1 > int2) {
+        let temp = int1 //switching round
+        int1 = int2
+        int2 = temp
+    }
+    let currentSum = 0;
+    for (i = int1; i <= int2; i++) {
+        currentSum += i
+    }
+    return currentSum
 };
 
 // Do not edit below this line
