6e097bb877a8ee4e1566cdb1a74b5d48107c116e
Dan <58478165+DKNTT@users.noreply.github.com>
2024-05-18 16:44:00 +0100

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let newString = '';
+    for (i = 1; i < string.length + 1; i++) {
+        newString += string.at(-i);
+    }
+    return newString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
+
