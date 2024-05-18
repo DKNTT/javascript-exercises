06edbb18444a45b86a4e3c7841e5b52d718b3d7b
Dan <58478165+DKNTT@users.noreply.github.com>
2024-05-18 18:13:08 +0100

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeThis) {
 
-};
+    for (i = 0; i < array.length;) {
+        removedElement = false;
+        for (j = 0; j < removeThis.length; j++) {
+            if (array[i] === removeThis[j]) {
+                array.splice(i, 1);
+                removedElement = true;
+            }
+        }
+        if (!removedElement) {
+            i++;
+        }
+    };
+    return array
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
